414e97f93396b4e9fd2317f173b6eeee2fea3da7
Kaussik V <kaussikvk@gmail.com>
2026-04-17 14:08:46 +0530

    kaussik JS

---
+++ task1.js
@@ -0,0 +1,98 @@
+//task 1:
+
+var val = 50
+val=100
+console.log(val);
+
+//task2:
+
+let marks = 80
+marks = 80+10
+console.log(marks);
+
+//task3 :
+
+const price = 500
+finalprice = price + 100
+console.log(finalprice);
+
+//task4 :
+
+let str= "Welcome Team"
+let num = 2026
+let boo = true
+
+console.log(str);
+console.log(num);
+console.log(boo);
+
+//task5:
+
+let strr= "JavaScript"
+let numm = 250
+let booo = false
+
+console.log(typeof(strr));
+console.log(typeof(numm));
+console.log(typeof(booo));
+
+//task6:
+
+let arr = ["apple","orange","banana","cherry","grapes"]
+
+console.log(arr);
+console.log(arr[0]);
+console.log(arr[arr.length-1]);
+
+//task7:
+
+let student = {
+ name: "Naveen",
+ age: 20,
+ course: "MERN"
+}
+
+console.log(student.name);
+console.log(student.age);
+
+//task8:
+
+console.log(20 + 10);
+console.log(50 - 25);
+console.log(5 * 5);
+console.log(100 / 4);
+console.log(20 % 3);
+
+//task9:
+
+let x = 5;
+x++;
+console.log(x);
+
+let y = 10;
+y--;
+console.log(y);
+
+//task10:
+
+console.log(10 > 5);
+console.log(5 < 2);
+console.log(20 == "20");
+console.log(15 === "15");
+console.log(10 != 8);
+
+//task11:
+
+console.log(5 > 2 && 10 > 3);
+console.log(7 < 5 || 8 > 2);
+console.log(!(10 > 5));
+
+//task12:
+
+let age = 18
+if (age >= 18){
+    console.log("Eligible");}
+ else{
+    console.log("Not eligible");
+    
+ }
